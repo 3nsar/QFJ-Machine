23c96a6bdf47fa9ee842af790e4e9ded681659a4
3nsar <84131267+3nsar@users.noreply.github.com>
2022-09-22 18:49:17 +0200

    responsive nav but it needs an update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const[showLinks, setShowLinks]= useState(true);
   return (
     <div className='nav-container'>
         <div className='nav-content'>
           <h1 className='title'>QFJ-MACHINE</h1>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><a href="#about">About</a></li>
-                <li><Link to="/quotes">Quotes</Link></li>
-                <li><Link to="/facts">Facts</Link></li>
-                <li><Link to="/jokes">Jokes</Link></li>
+          <div className='links' >
+            <ul id={showLinks ? "hidden" : ""}>
+                <li><Link to="/" onClick={()=> setShowLinks(!showLinks)}>Home</Link></li>
+                <li><a href="/#about" onClick={()=> setShowLinks(!showLinks)}>About</a></li>
+                <li><Link to="/quotes" onClick={()=> setShowLinks(!showLinks)}>Quotes</Link></li>
+                <li><Link to="/facts" onClick={()=> setShowLinks(!showLinks)}>Facts</Link></li>
+                <li><Link to="/jokes"  onClick={()=> setShowLinks(!showLinks)}>Jokes</Link></li>
             </ul>
+            </div>
+            <button className='burger' onClick={()=> setShowLinks(!showLinks)}>Open</button>
         </div>
     </div>
   )
