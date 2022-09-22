077fb52f82311d11f1aecd2f2b4fbf49ea00f218
3nsar <84131267+3nsar@users.noreply.github.com>
2022-09-22 19:55:54 +0200

    updated nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className='nav-container'>
         <div className='nav-content'>
           <h1 className='title'>QFJ-MACHINE</h1>
-          <div className='links' >
+          
             <ul id={showLinks ? "hidden" : ""}>
                 <li><Link to="/" onClick={()=> setShowLinks(!showLinks)}>Home</Link></li>
                 <li><a href="/#about" onClick={()=> setShowLinks(!showLinks)}>About</a></li>
@@ -15,8 +15,7 @@ const Navbar = () => {
                 <li><Link to="/facts" onClick={()=> setShowLinks(!showLinks)}>Facts</Link></li>
                 <li><Link to="/jokes"  onClick={()=> setShowLinks(!showLinks)}>Jokes</Link></li>
             </ul>
-            </div>
-            <button className='burger' onClick={()=> setShowLinks(!showLinks)}>Open</button>
+          <button className='burger' onClick={()=> setShowLinks(!showLinks)}>Open</button> 
         </div>
     </div>
   )
