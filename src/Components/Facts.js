066a8735b24b7b1b681e63b8764d208287582be6
3nsar <84131267+3nsar@users.noreply.github.com>
2022-09-24 20:43:42 +0200

    changed some thing

--- src/Components/Facts.js
+++ src/Components/Facts.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import ClipLoader from "react-spinners/ScaleLoader";
-import Footer from './Footer';
+
 
 const Facts = () => {
 
